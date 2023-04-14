5fe8333417bd251d2045cd2f93a21363bc314e8d
zjx <1098505411@qq.com>
2023-04-14 13:46:32 +0800

    部分修改

---
+++ AIops_BackEnd/src/main/java/com/aiops_web/entity/neo4j/Container.java
@@ -0,0 +1,20 @@
+package com.aiops_web.entity.neo4j;
+
+import lombok.Builder;
+import lombok.Data;
+import org.neo4j.ogm.annotation.*;
+
+@RelationshipEntity(type = "contains")
+@Data
+@Builder
+public class Container {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @StartNode
+    private System parent;
+
+    @EndNode
+    private Pod child;
+}
