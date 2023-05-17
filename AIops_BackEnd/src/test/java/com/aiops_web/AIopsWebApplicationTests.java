e9984007a3a5e0efbcee151d37350c19394d64f8
GaryDing01 <gding01@qq.com>
2023-05-17 18:20:38 +0800

    Update_Test
    Update_Test

--- AIops_BackEnd/src/test/java/com/aiops_web/AIopsWebApplicationTests.java
+++ AIops_BackEnd/src/test/java/com/aiops_web/AIopsWebApplicationTests.java
@@ -1,20 +1,11 @@
 package com.aiops_web;
 
-import com.aiops_web.dao.sql.AiopsAlgMapper;
-import com.aiops_web.entity.sql.AnomalyInfo;
-import com.aiops_web.entity.sql.User;
-import com.aiops_web.service.AiopsAlgService;
-import com.aiops_web.service.AnomalyInfoService;
-import com.aiops_web.service.UserService;
-import com.aiops_web.service.impl.AiopsAlgServiceImpl;
-import com.aiops_web.std.JWTUtils;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.aiops_web.service.mysql.AnomalyInfoService;
+import com.aiops_web.service.mysql.UserService;
+import com.aiops_web.service.mysql.impl.AiopsAlgServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Date;
-import java.util.List;
 
 @SpringBootTest
 class AIopsWebApplicationTests {
